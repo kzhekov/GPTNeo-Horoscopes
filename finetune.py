aaf86f6f1278a84ddb83ce092ebd3dcbb6919050
Kamen Zhekov <kzh@odoo.com>
2021-06-26 11:42:11 +0200

    Adding fine-tuning based on clean horoscope file

---
+++ finetune.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, random_split
+from transformers import GPT2Tokenizer, TrainingArguments, Trainer, GPTNeoForCausalLM
+
+torch.manual_seed(42)
+tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M", bos_token='<|startoftext|>',
+                                          eos_token='<|endoftext|>', pad_token='<|pad|>')
+model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M").cuda()
+model.resize_token_embeddings(len(tokenizer))
+descriptions = pd.read_csv('horoscopes_all_clean.csv', delimiter = "\n", header=None)[0]
+max_length = max([len(tokenizer.encode(description)) for description in descriptions])
+
+
+class HoroscopeDataset(Dataset):
+    def __init__(self, txt_list, tokenizer, max_length):
+        self.input_ids = []
+        self.attn_masks = []
+        self.labels = []
+        for txt in txt_list:
+            encodings_dict = tokenizer('<|startoftext|>' + txt + '<|endoftext|>', truncation=True,
+                                       max_length=max_length, padding="max_length")
+            self.input_ids.append(torch.tensor(encodings_dict['input_ids']))
+            self.attn_masks.append(torch.tensor(encodings_dict['attention_mask']))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.attn_masks[idx]
+
+
+dataset = HoroscopeDataset(descriptions, tokenizer, max_length=max_length)
+train_size = int(0.9 * len(dataset))
+train_dataset, val_dataset = random_split(dataset, [train_size, len(dataset) - train_size])
+training_args = TrainingArguments(output_dir='./results', num_train_epochs=5, logging_steps=500, save_steps=5000,
+                                  per_device_train_batch_size=1, per_device_eval_batch_size=1,
+                                  warmup_steps=100, weight_decay=0.01, logging_dir='./logs')
+Trainer(model=model, args=training_args, train_dataset=train_dataset,
+        eval_dataset=val_dataset, data_collator=lambda data: {'input_ids': torch.stack([f[0] for f in data]),
+                                                              'attention_mask': torch.stack([f[1] for f in data]),
+                                                              'labels': torch.stack([f[0] for f in data])}).train()
+generated = tokenizer("<|startoftext|> ", return_tensors="pt").input_ids.cuda()
+sample_outputs = model.generate(generated, do_sample=True, top_k=50, 
+                                max_length=300, top_p=0.95, temperature=1.9, num_return_sequences=20)
+for i, sample_output in enumerate(sample_outputs):
+    print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
