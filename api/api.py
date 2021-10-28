42bdec68d65d5f02e942557982f088923d979e3e
Kamen Zhekov <kamenzhekovu@gmail.com>
2021-10-28 10:50:31 +0200

    Adding CORS configuration for API

--- api/api.py
+++ api/api.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from horoscope_model import HoroscopeModel, get_model
 
 app = FastAPI()
+origins = ["*"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class HoroscopeRequest(BaseModel):
     horoscope_id: int
